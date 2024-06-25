be08537114339ee0c805c96a732fbd7189584dec
Devon Sawatsky <devon@nove.dev>
2024-06-25 12:35:16 -0700

    add support for multichar lexemes

--- src/main/java/dev/nove/jlox/Scanner.java
+++ src/main/java/dev/nove/jlox/Scanner.java
@@ -60,11 +60,61 @@ class Scanner {
             case '*':
                 addToken(STAR);
                 break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) {
+                        advance();
+                    }
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+
+            case '\n':
+                line++;
+                break;
+
             default:
                 Lox.error(line, "Unexpected character.");
                 // todo: don't spam the user with a billion errors if there's a lot of them in a
                 // row
                 break;
+        }
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) {
+            return false;
+        } else if (source.charAt(current) != expected) {
+            return false;
+        } else {
+            current++;
+            return true;
+        }
+    }
+
+    private char peek() {
+        if (isAtEnd()) {
+            return '\0';
+        } else {
+            return source.charAt(current);
         }
     }
 
