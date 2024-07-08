ff83be1a08afcfd3d49c40833d6ebc25803c80f4
Devon Sawatsky <devon@nove.dev>
2024-07-08 16:19:24 -0700

    generate statement

--- src/main/java/dev/nove/jlox/tool/GenerateAst.java
+++ src/main/java/dev/nove/jlox/tool/GenerateAst.java
@@ -14,9 +14,14 @@ public class GenerateAst {
             String outputDir = args[0];
             defineAst(outputDir, "Expr", Arrays.asList(
                     "Binary : Expr left, Token operator, Expr right",
+                    "Ternary : Expr condition, Expr ifTrue, Expr ifFalse",
                     "Grouping    : Expr expression",
                     "Literal     : Object value",
                     "Unary       : Token operator, Expr right"));
+
+            defineAst(outputDir, "Stmt", Arrays.asList(
+                    "Expression : Expr expression",
+                    "Print           : Expr expression"));
         }
     }
 
